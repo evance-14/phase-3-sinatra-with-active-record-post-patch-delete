58f64d113542e866079619f77176d85c427933c3
evance-14 <kiropevans097@gmail.com>
2023-02-27 20:51:08 +0300

    completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
@@ -14,6 +16,35 @@ class ApplicationController < Sinatra::Base
         user: { only: [:name] }
       } }
     })
+  end
+
+  delete '/reviews/:id' do
+    #find review using id
+    review = Review.find(params[:id])
+    #delete the review
+    review.destroy
+    #send a response with deleted review as JSON
+    review.to_json
+  end
+
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
   end
 
 end
